43fabce8b0447b42f8a448677da23d526a6a97f5
DianaDob <dobsinszki.diana@gmail.com>
2022-07-17 11:36:33 +0200

    feat: change picture if different date is picked

--- src/main/resources/static/js/apod.js
+++ src/main/resources/static/js/apod.js
@@ -2,7 +2,14 @@ let title = document.getElementById("title");
 let image = document.getElementById("image");
 let description = document.getElementById("description");
 
+
+let datePicker = document.getElementById("date_input");
+datePicker.max = new Date().toISOString().split("T")[0];
+
+
+datePicker.addEventListener("change", fetchEarlierData);
 fetchData();
+
 
 function updatePageWithData(data) {
     title.textContent = data.title;
@@ -12,6 +19,13 @@ function updatePageWithData(data) {
 
 function fetchData(){
     fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+        .then(response => response.json())
+        .then(data => updatePageWithData(data));
+}
+
+function fetchEarlierData() {
+    let date = datePicker.value;
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
         .then(response => response.json())
         .then(data => updatePageWithData(data));
 }
